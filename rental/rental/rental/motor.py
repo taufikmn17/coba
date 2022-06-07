1051b4102266bebce2c9ebda032fc76046c29a7c
taufikmn17 <105372767+taufikmn17@users.noreply.github.com>
2022-06-07 19:46:11 +0700

    Add files via upload

---
+++ rental/rental/rental/motor.py
@@ -0,0 +1,7 @@
+class Motor:
+    def __init__(self, tipemotor, merek):
+        self.tipemotor = tipemotor
+        self.merek = merek
+
+    def pilihtipe(self):
+        print("==++ Silakan Pilih Tipe Kendaraan ++==\n")
