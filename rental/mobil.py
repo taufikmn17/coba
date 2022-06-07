4ba5d48a7bd3d9072832f6e41d25bcee05e7dbfb
taufikmn17 <105372767+taufikmn17@users.noreply.github.com>
2022-06-07 19:44:33 +0700

    Delete rental directory

--- rental/mobil.py
+++
@@ -1,7 +0,0 @@
-class Mobil:
-    def __init__(self, tipemobil, merek):
-        self.tipemobil = tipemobil
-        self.merek = merek
-
-    def pilihtipe(self):
-        print("==++ Silakan Pilih Tipe Kendaraan ++==\n")
--- rental/motor.py
+++
@@ -1,7 +0,0 @@
-class Motor:
-    def __init__(self, tipemotor, merek):
-        self.tipemotor = tipemotor
-        self.merek = merek
-
-    def pilihtipe(self):
-        print("==++ Silakan Pilih Tipe Kendaraan ++==\n")
